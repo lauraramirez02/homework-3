cc7a50f85ef9a9f5342fe542fc753c932c66be82
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-02 16:03:35 -0700

    6th

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ for (const dept of org1_depts){
 }
 output += end_tag;
 
-for (let a=0, b=org1_depts.length; a<b; a++) {
+for (let a = 0, b = org1_depts.length; a < b; a++) {
     var objNF= JSON.stringify(org1_depts[a].name);
     var objF= objNF.replace(/\"/g, "")
     output += start_li + objF;
@@ -31,22 +31,5 @@ for (let a=0, b=org1_depts.length; a<b; a++) {
 output += end_li
 output += end_tag
 
-const outputDiv= document.getElementById('output')
-outputDiv.innerHTML= output
-
-const org2_depts = [
-    {
-        name: 'accounting',
-        children: [
-            { name: 'accounting payable', children: [] },
-            {
-                name: 'accounting receivable',
-                children: [{ name: 'cash', children: [] }, { name: 'check', children: [] }],
-            },
-        ],
-    },
-    {
-        name: 'finance',
-        children: [{ name: 'investment', children: [] }],
-    },
-]
+const outputDiv = document.getElementById('output')
+outputDiv.innerHTML = output
