782236e0588095788685534d2f9e1a95bfe9de83
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-02 16:21:21 -0700

    8th

--- app.js
+++ app.js
@@ -11,23 +11,25 @@ const org1_depts = [
         children: [],
     },
 ]
-const start_tag = '<ol>'
-const end_tag = '</ol>'
+
+const start_tag = '<ul>'
+const end_tag = '</ul>'
 const start_li = '<li>'
 const end_li = '</li>'
-var output = start_tag
 
-//for (const dept of org1_depts){
-  //  output += start_li + dept.name + end_li
-//}
-//output += end_tag;
-
-for (let a = 0, b = org1_depts.length; a < b; a++) {
-    var objNF = JSON.stringify(org1_depts[a].name);
-    var objF = objNF.replace(/\"/g, "")
-    output += start_li + objF;
+let output = start_tag
+for (const dept of org1_depts){
+  output += start_li + dept.name + end_li
 }
-output += end_li
+output += end_tag;
+
+//for (let a = 0, b = org1_depts.length; a < b; a++) {
+    //let objNF = JSON.stringify(org1_depts[a].name);
+    //let objF = objNF.replace(/\"/g, "")
+    //output += start_li + objF;
+//}
+
+//output += end_li
 output += end_tag
 
 const outputDiv = document.getElementById('output')
