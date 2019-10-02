be9abc45b16a4ff6f7778d84654012442847208b
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-02 16:10:01 -0700

    7th

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const org1_depts = [
     {
         name: 'finance',
         children: [],
-
     },
 ]
 const start_tag = '<ol>'
@@ -18,14 +17,14 @@ const start_li = '<li>'
 const end_li = '</li>'
 var output = start_tag
 
-for (const dept of org1_depts){
-    output += start_li + dept.name + end_li
-}
-output += end_tag;
+//for (const dept of org1_depts){
+  //  output += start_li + dept.name + end_li
+//}
+//output += end_tag;
 
 for (let a = 0, b = org1_depts.length; a < b; a++) {
-    var objNF= JSON.stringify(org1_depts[a].name);
-    var objF= objNF.replace(/\"/g, "")
+    var objNF = JSON.stringify(org1_depts[a].name);
+    var objF = objNF.replace(/\"/g, "")
     output += start_li + objF;
 }
 output += end_li
