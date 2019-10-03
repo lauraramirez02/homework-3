b76690e3f1aa7fa24b21641b89553884e195068b
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-02 18:51:52 -0700

    initial

--- app.js
+++ app.js
@@ -12,43 +12,14 @@ const org1_depts = [
     },
 ]
 
-const start_tag = '<ul>'
-const end_tag = '</ul>'
-const start_li = '<li>'
-const end_li = '</li>'
+var list = document.getElementsByTagName ('ul')[0];
 
-let output = start_tag
-for (const dept of org1_depts){
-  output += start_li + dept.name + end_li
-}
-output += end_tag;
+var newItemLast = document.createTextNode('li');
+var newTextLast = document.createTextNode('accounting');
+newItemLast.appendChild(newTextLast);
+list.appendChild(newItemLast);
 
-
-for (let a = 0, b = org1_depts.length; a < b; a++) {
-    let objNF = JSON.stringify(org1_depts[a].name);
-    let objF = objNF.replace(/\"/g, "")
-    output += start_li + objF;
-}
-
-output += end_li
-output += end_tag
-
-const outputDiv = document.getElementById('output')
-outputDiv.innerHTML = output
-
-const org2_depts = [
-    {
-        name: 'accounting',
-        children: [
-            { name: 'accounting payable', children: [] },
-            {
-                name: 'accounting receivable',
-                children: [{ name: 'cash', children: [] }, { name: 'check', children: [] }],
-            },
-        ],
-    },
-    {
-        name: 'finance',
-        children: [{ name: 'investment', children: [] }],
-    },
-]
+var newFirst = document.createElement('li');
+var newTextFirst = document.createTextNode('finance');
+newItemFirst.appendChild(newTextFirst);
+list.insertBefore(newItemFirst, list.firstChild);
