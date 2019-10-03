eca2f5b1ee46ab1bc0954e56f9f25c97b70052ce
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-02 20:56:36 -0700

    7th

--- app.js
+++ app.js
@@ -28,32 +28,31 @@ const org2_depts=[
         children: [{name: 'investment', children: []}],
     },
 ]
-
 function PrintDepts(depts) {
     for (let i = 0; i < depts.length; i++) {
         let x = depts[i];
         let docul = document.createElement('ul');
-        let newlistitem=document.createElement('li');
-        let listvalue=document.createTextNode(x.name);
-        newlistitem.appendChild(listvalue);
+        let newListItem=document.createElement('li');
+        let listValue=document.createTextNode(x.name);
+        newListItem.appendChild(listvalue);
         docul.appendChild(newlistitem);
         if (x.children.length > 1) {
             for (let i2 = 0; i2 < x.children.length; i2++) {
                 let y = x.children[i2]
                 let docul2 = document.createElement('ul');
                 docul.appendChild(docul2);
-                let newlistitem2=document.createElement('li');
-                let listvalue2=document.createTextNode(y.name);
-                newlistitem2.appendChild(listvalue2);
+                let newListItem2=document.createElement('li');
+                let listValue2=document.createTextNode(y.name);
+                newListItem2.appendChild(listvalue2);
                 docul2.appendChild(newlistitem2);
                 if (y.children.length > 1) {
                     for (let i3 = 0; i3 < y.children.length; i3++) {
                         let z = y.children[i3];
                         let docul3 = document.createElement('ul');
                         docul2.appendChild(docul3);
-                        let newlistitem3=document.createElement('li');
-                        let listvalue3=document.createTextNode(z.name);
-                        newlistitem3.appendChild(listvalue3);
+                        let newListItem3=document.createElement('li');
+                        let ListValue3=document.createTextNode(z.name);
+                        newListItem3.appendChild(listvalue3);
                         docul3.appendChild(newlistitem3);
                     }
                 }
@@ -65,5 +64,5 @@ function PrintDepts(depts) {
 
 }
 
-PrintDepts(org1_depts);
-PrintDepts(org2_depts);
+printDepts(org1_depts);
+printDepts(org2_depts);
